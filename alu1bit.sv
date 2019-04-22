4fb60d331c90777cf78a299e4223574af8ac86fe
Yiftach Edelstain <yiftach.ede@campus.technion.ac.il>
2019-04-22 15:36:25 +0300

    finished alu1bit prior to testing

--- alu1bit.sv
+++ alu1bit.sv
@@ -10,7 +10,24 @@ module alu1bit (
 
 // Put your code here
 // ------------------
+logic nor_o,nand_o,add_o,sub_o,arithm_o;
+logic and_t;
+logic add_cout, sub_cout;
+// nand
 
+AND2 and_inst(.Z(and_t),.A(a),.B(b));
+NOR2 flip_and_inst(.Z(nand_o),.A(and_t),.B(and_t));
+// nor
+NOR2 nor_inst(.Z(nor_o),.A(a),.B(b));
+
+// add / sub 
+fas add_inst(.cout(add_cout),.s(add_o),.cin(cin),.a(a),.b(b),.a_ns(op[0]));
+fas add_inst(.cout(sub_cout),.s(sub_o),.cin(cin),.a(a),.b(b),.a_ns(op[0]));
+// cout decision
+mux2 mux_cout_inst(.z(cout),.sel(op[0]),.d0(add_cout),.d1(sub_cout));
+
+// end choice
+mux4 mux_inst(.z(s),.d0(nand_o),.d1(nor_o),.d2(add_o),.d3(sub_o),.sel(op));
 
 // End of your code
 
