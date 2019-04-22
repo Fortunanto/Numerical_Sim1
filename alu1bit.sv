398e0b77433bb0bedeffddca130b8fcdb172595d
Yiftach Edelstain <yiftach.ede@campus.technion.ac.il>
2019-04-22 16:11:15 +0300

    made changes to alu1bit impl

--- alu1bit.sv
+++ alu1bit.sv
@@ -10,24 +10,20 @@ module alu1bit (
 
 // Put your code here
 // ------------------
-logic nor_o,nand_o,add_o,sub_o,arithm_o;
+logic nor_o,nand_o,res_o;
 logic and_t;
-logic add_cout, sub_cout;
 // nand
 
-AND2 and_inst(.Z(and_t),.A(a),.B(b));
-NOR2 flip_and_inst(.Z(nand_o),.A(and_t),.B(and_t));
+AND2 #(.Tpdlh(2),.Tpdhl(12)) and_inst(.Z(and_t),.A(a),.B(b));
+NOR2 #(.Tpdlh(10),.Tpdhl(8)) flip_and_inst(.Z(nand_o),.A(and_t),.B(and_t));
 // nor
-NOR2 nor_inst(.Z(nor_o),.A(a),.B(b));
+NOR2 #(.Tpdlh(10),.Tpdhl(8)) nor_inst(.Z(nor_o),.A(a),.B(b));
 
 // add / sub 
-fas add_inst(.cout(add_cout),.s(add_o),.cin(cin),.a(a),.b(b),.a_ns(op[0]));
-fas add_inst(.cout(sub_cout),.s(sub_o),.cin(cin),.a(a),.b(b),.a_ns(op[0]));
-// cout decision
-mux2 mux_cout_inst(.z(cout),.sel(op[0]),.d0(add_cout),.d1(sub_cout));
+fas add_inst(.cout(cout),.s(res_o),.cin(cin),.a(a),.b(b),.a_ns(op[0]));
 
 // end choice
-mux4 mux_inst(.z(s),.d0(nand_o),.d1(nor_o),.d2(add_o),.d3(sub_o),.sel(op));
+mux4 mux_inst(.z(s),.d0(nand_o),.d1(nor_o),.d2(res_o),.d3(res_0),.sel(op));
 
 // End of your code
 
